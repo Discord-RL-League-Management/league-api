be006e77bb48e8733c47b96ece97168ce1751c7d
kpreilly <reilly.kyle101@gmail.com>
2025-10-26 21:14:39 -0700

    fix(discord): add CacheModule to DiscordModule for validation service

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 import { DiscordApiService } from './discord-api.service';
 import { DiscordValidationService } from './discord-validation.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule],
   providers: [DiscordApiService, DiscordValidationService],
   exports: [DiscordApiService, DiscordValidationService],
 })
