1fb41e438774c1d1ebfe74e2ea4f1d66a365bca1
kpreilly <reilly.kyle101@gmail.com>
2025-10-31 15:08:47 -0700

    feat(users): add user exceptions

---
+++ src/users/exceptions/user.exceptions.ts
@@ -0,0 +1,19 @@
+import { NotFoundException, ConflictException } from '../../common/exceptions/base.exception';
+
+/**
+ * UserNotFoundException - Domain-specific exception for user not found
+ */
+export class UserNotFoundException extends NotFoundException {
+  constructor(userId: string) {
+    super('User', userId);
+  }
+}
+
+/**
+ * UserAlreadyExistsException - Domain-specific exception for duplicate user
+ */
+export class UserAlreadyExistsException extends ConflictException {
+  constructor(userId: string) {
+    super(`User with ID '${userId}' already exists`, { userId });
+  }
+}
